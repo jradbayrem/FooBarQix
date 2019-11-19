06bad582e111d7e58e4d36d2bf5beeebd542d2c8
Jrad Bayrem <jrad.bayrem@hotmail.fr>
2019-11-19 11:33:57 +0100

    test should_return_Bar_when_divisable_by_5: OK

--- src/main/java/io/wesquad/FooBarQix/FooBarQix.java
+++ src/main/java/io/wesquad/FooBarQix/FooBarQix.java
@@ -2,19 +2,36 @@ package io.wesquad.FooBarQix;
 
 public class FooBarQix {
 	
+	public FooBarQix() {
+		this.result = new StringBuilder();
+	}
+	
+	private StringBuilder result;
 	
 	public String compute(String paramToAnalyse) {
 	
 		int transformedParam = Integer.parseInt(paramToAnalyse);
 		
 		if((transformedParam % 3) == 0) {
-			return "Foo";
+			this.result.append( "Foo");
+		}
+		if((transformedParam % 5) == 0) {
+			this.result.append( "Bar");
 		}
 		
-		return null;
+		return this.result.toString();
+	}
+
+	public StringBuilder getResult() {
+		return result;
+	}
+
+	public void setResult(StringBuilder result) {
+		this.result = result;
 	}
 	
 	
-
+	
+	
 
 }
