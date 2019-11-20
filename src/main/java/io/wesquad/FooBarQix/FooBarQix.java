f791722e1608b3fb0fa4f3c3726b7ee3830c606f
Jrad Bayrem <jrad.bayrem@hotmail.fr>
2019-11-20 13:37:36 +0100

    test should_return_1Star1_when_having_101:OK

--- src/main/java/io/wesquad/FooBarQix/FooBarQix.java
+++ src/main/java/io/wesquad/FooBarQix/FooBarQix.java
@@ -22,21 +22,23 @@ public class FooBarQix {
 			this.result.append("Qix");
 		}
 
-		if((this.result.length() == 0) && (transformedParam < 10)) {
-			this.result.append(transformedParam);
-		}
 		paramToAnalyse.chars().mapToObj(subject -> (char) subject)
-				.filter(candidate -> (candidate == '3' || candidate == '5' || candidate == '7')).forEach(element -> {
+				.filter(candidate -> (candidate == '3' || candidate == '5' || candidate == '7' || candidate == '0')).forEach(element -> {
 					if (element == '3') {
 						this.result.append("Foo");
 					} else if (element == '5') {
 						this.result.append("Bar");
 					} else if (element == '7') {
 						this.result.append("Qix");
+					}else {
+						this.result.append("0");
 					}
 				});
 
-		return this.result.toString();
+		if((this.result.length() == 0) || (this.result.length() == this.result.chars().filter(candidate -> candidate == '0').count())) {
+			this.result = new StringBuilder().append(transformedParam);
+		}
+		return this.result.toString().replace('0', '*');
 	}
 
 	public StringBuilder getResult() {
