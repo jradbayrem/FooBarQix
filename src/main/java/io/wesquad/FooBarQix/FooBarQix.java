c6adacff26c9d5d1cfbcd16c00562af4649b937f
Jrad Bayrem <jrad.bayrem@hotmail.fr>
2019-11-22 19:11:00 +0100

    Refactoring methode compute in FooBarQix and test: OK

--- src/main/java/io/wesquad/FooBarQix/FooBarQix.java
+++ src/main/java/io/wesquad/FooBarQix/FooBarQix.java
@@ -1,52 +1,22 @@
 package io.wesquad.FooBarQix;
 
+import io.wesquad.FooBarQix.strategy.impl.ComputingStrategies;
+import io.wesquad.FooBarQix.strategy.impl.ContainsStrategy;
+import io.wesquad.FooBarQix.strategy.impl.DivideStrategy;
+
 public class FooBarQix {
 
-	public FooBarQix() {
-		this.result = new StringBuilder();
-	}
-
-	private StringBuilder result;
+	private ContainsStrategy containsStrategy;
+	private DivideStrategy divideStrategy;
+	private ComputingStrategies computingStrategies;
 
 	public String compute(String paramToAnalyse) {
 
-		int transformedParam = Integer.parseInt(paramToAnalyse);
+		this.containsStrategy = new ContainsStrategy();
+		this.divideStrategy = new DivideStrategy();
+		this.computingStrategies = new ComputingStrategies(divideStrategy, containsStrategy);
+		return this.computingStrategies.compute(paramToAnalyse);
 
-		if ((transformedParam % 3) == 0) {
-			this.result.append("Foo");
-		}
-		if ((transformedParam % 5) == 0) {
-			this.result.append("Bar");
-		}
-		if ((transformedParam % 7) == 0) {
-			this.result.append("Qix");
-		}
-
-		paramToAnalyse.chars().mapToObj(subject -> (char) subject)
-				.filter(candidate -> (candidate == '3' || candidate == '5' || candidate == '7' || candidate == '0')).forEach(element -> {
-					if (element == '3') {
-						this.result.append("Foo");
-					} else if (element == '5') {
-						this.result.append("Bar");
-					} else if (element == '7') {
-						this.result.append("Qix");
-					}else {
-						this.result.append("0");
-					}
-				});
-
-		if((this.result.length() == 0) || (this.result.length() == this.result.chars().filter(candidate -> candidate == '0').count())) {
-			this.result = new StringBuilder().append(transformedParam);
-		}
-		return this.result.toString().replace('0', '*');
-	}
-
-	public StringBuilder getResult() {
-		return result;
-	}
-
-	public void setResult(StringBuilder result) {
-		this.result = result;
 	}
 
 }
